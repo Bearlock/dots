5369c76aca80d3c9b8ba5de6a22b090a9a7f1a0b
Erick Diaz <ediaz@bamboohr.com>
2023-08-30 09:38:04 -0600

    Delete private_dot_config directory

--- private_dot_config/nvim/lua/keymaps.lua
+++
@@ -1,6 +0,0 @@
-require('keymaps.coc-nvim')
-require('keymaps.telescope-nvim')
-require('keymaps.tagbar')
-require('keymaps.neogen')
-require('keymaps.toggleterm-nvim')
-require('keymaps.neorg')
--- private_dot_config/nvim/lua/keymaps/tagbar.lua
+++
@@ -1,4 +0,0 @@
-local map = vim.keymap.set
-
--- Open up a sidebar with the slurped in tags
-map('n', '<F8>', '<cmd>TagbarToggle<cr>', {})
